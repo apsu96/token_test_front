bc0a999d9e2667377888938c4d96b0370fd8411c
Svetlana Apsalikova <apsu96@bk.ru>
2022-05-19 18:54:16 +0400

    Add acount details card

--- token_test_front/token_test_front/src/MetamaskConnect.js
+++ token_test_front/token_test_front/src/MetamaskConnect.js
@@ -1,10 +1,10 @@
-import { Box, Typography,Container,CssBaseline, Button } from "@mui/material";
+import { Box, Typography,Container,CssBaseline, Button, TextField } from "@mui/material";
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 
 function MetamaskConnect() {
-    const [account, setAccount] = useState(null);
-    const [balance, setBalance] = useState(null);
+    const [account, setAccount] = useState("address");
+    const [balance, setBalance] = useState("balance");
     const [errorMessage, setErrorMessage] = useState(null);
 
     function connectHandler() {
@@ -30,6 +30,16 @@ function MetamaskConnect() {
             <Typography variant="h5">Connect to your Metamask account</Typography>
             <Button variant="contained" sx={{ mt: 3, mb: 2 }}onClick={connectHandler}>Connect</Button>
             <Typography variant="subtitle2" color="red">{errorMessage}</Typography>
+        </Box>
+        <Box sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+        }}>
+            <Typography variant="h5">Your account details</Typography>
+            <TextField value={"Address:  " + account} margin="dense" sx={{padding: "auto", }}></TextField>
+            <TextField value={"Balance:  " + balance} margin="dense"></TextField>
         </Box>
     </Container>
 }
