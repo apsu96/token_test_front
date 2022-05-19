980e65c0fb538627123413a96b1f9b8ba3848fbd
Svetlana Apsalikova <apsu96@bk.ru>
2022-05-19 18:30:51 +0400

    Add connect handler function in MetamaskConnect component

--- token_test_front/token_test_front/src/MetamaskConnect.js
+++ token_test_front/token_test_front/src/MetamaskConnect.js
@@ -1,6 +1,23 @@
 import { Box, Typography,Container,CssBaseline, Button } from "@mui/material";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
 
 function MetamaskConnect() {
+    const [account, setAccount] = useState(null);
+    const [balance, setBalance] = useState(null);
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    function connectHandler() {
+        if (window.ethereum) {
+            window.ethereum.request({
+                method: "eth_requestAccounts",
+            }).then(result => {
+                
+            })
+        } else {
+            setErrorMessage("Please install MetaMask");    
+        }
+    }
 
     return  <Container component="main" maxWidth="xs">
         <CssBaseline />
@@ -11,7 +28,8 @@ function MetamaskConnect() {
             alignItems: "center",
         }}>
             <Typography variant="h5">Connect to your Metamask account</Typography>
-            <Button variant="contained" sx={{ mt: 3, mb: 2 }}>Connect</Button>
+            <Button variant="contained" sx={{ mt: 3, mb: 2 }}onClick={connectHandler}>Connect</Button>
+            <Typography variant="subtitle2" color="red">{errorMessage}</Typography>
         </Box>
     </Container>
 }
