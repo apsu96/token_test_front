555a7ff1455c1cc87e3cf959f9e3478b651d3f3c
Svetlana Apsalikova <apsu96@bk.ru>
2022-05-22 23:34:27 +0400

    Change transfer form

--- token_test_front/token_test_front/src/MetamaskConnect.js
+++ token_test_front/token_test_front/src/MetamaskConnect.js
@@ -1,4 +1,17 @@
-import { Box, Typography,Button, Modal, Backdrop, CircularProgress, Card,CardContent, Table, TableBody, TableRow, TableCell } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Button,
+  Modal,
+  Backdrop,
+  CircularProgress,
+  Card,
+  CardContent,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@mui/material";
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 import TokenAbi from "./abi/TokenAbi.json";
@@ -7,163 +20,236 @@ import TransferForm from "./TransferForm";
 const TOKEN_ADDRESS = "0xf7DC76fbE6D64dcf7C131B1b9b4213B2AFF494d5";
 
 function MetamaskConnect() {
-    const [account, setAccount] = useState(null);
-    const [balance, setBalance] = useState(null);
-    const [decimals, setDecimals] = useState(null);
-    const [name, setName] = useState(null);
-    const [symbol, setSymbol] = useState(null);
-    const [totalSupply, setTotalSupply] = useState(null);
-    const [isActive, setIsActive] = useState(false);
-    const [errorMessage, setErrorMessage] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
+  const [account, setAccount] = useState(null);
+  const [balance, setBalance] = useState(null);
+  const [decimals, setDecimals] = useState(null);
+  const [name, setName] = useState(null);
+  const [symbol, setSymbol] = useState(null);
+  const [totalSupply, setTotalSupply] = useState(null);
+  const [isActive, setIsActive] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        if (window.ethereum) {
-            window.ethereum.on("accountsChanged", accountChangedHandler);
-            window.ethereum.on("chainChanged", chainChangedHandler);
-        } else {
-            setErrorMessage("Please install MetaMask");
-        }
-        return () => {
-            if (window.ethereum) {
-              window.ethereum.removeListener('accountsChanged', accountChangedHandler)
-              window.ethereum.removeListener('chainChanged', chainChangedHandler)
-            }
-          }
-    }, [])
-
-    function connectHandler() {
-        setIsLoading(true);
-        window.ethereum.request({method: "eth_requestAccounts"}).then(res => {
-            setAccount(res);
-            getToken(res);
-        })  
+  useEffect(() => {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", accountChangedHandler);
+      window.ethereum.on("chainChanged", chainChangedHandler);
+    } else {
+      setErrorMessage("Please install MetaMask");
     }
+    return () => {
+      if (window.ethereum) {
+        window.ethereum.removeListener(
+          "accountsChanged",
+          accountChangedHandler
+        );
+        window.ethereum.removeListener("chainChanged", chainChangedHandler);
+      }
+    };
+  }, []);
 
-    function getToken(res) {
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const signer = provider.getSigner();
-        const token = new ethers.Contract(TOKEN_ADDRESS, TokenAbi, signer);
-        token.balanceOf(res.toString()).then((res) => {
-            const tokenNum = Number(ethers.utils.formatEther(res));
-            setBalance(tokenNum.toFixed(2));
-        }).catch((err) => console.log(err));
-        token.name(account).then((res) => setName(res));
-        token.decimals(account).then(res => setDecimals(res));
-        token.symbol(account).then(res => setSymbol(res));
-        token.totalSupply(account).then(res => {
-            const supplyNum = Number(ethers.utils.formatEther(res));
-            setTotalSupply(supplyNum.toFixed(2));
-            setIsLoading(false);
-        });
+  function connectHandler() {
+    setIsLoading(true);
+    window.ethereum.request({ method: "eth_requestAccounts" }).then((res) => {
+      setAccount(res);
+      getToken(res);
+    });
+  }
+
+  function getToken(res) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const token = new ethers.Contract(TOKEN_ADDRESS, TokenAbi, signer);
+    token
+      .balanceOf(res.toString())
+      .then((res) => {
+        const tokenNum = Number(ethers.utils.formatEther(res));
+        setBalance(tokenNum.toFixed(2));
+      })
+      .catch((err) => console.log(err));
+    token.name(account).then((res) => setName(res));
+    token.decimals(account).then((res) => setDecimals(res));
+    token.symbol(account).then((res) => setSymbol(res));
+    token.totalSupply(account).then((res) => {
+      const supplyNum = Number(ethers.utils.formatEther(res));
+      setTotalSupply(supplyNum.toFixed(2));
+      setIsLoading(false);
+    });
+  }
+
+  function chainChangedHandler() {
+    window.location.reload();
+  }
+
+  function accountChangedHandler(accounts) {
+    setAccount(accounts[0]);
+    setBalance(null);
+    setDecimals(null);
+    setSymbol(null);
+    setTotalSupply(null);
+    setName(null);
+    getToken(accounts[0]);
+  }
+
+  function shortAddress(account) {
+    if (account) {
+      let substring1 = account.toString().slice(0, 5);
+      let substring2 = account.toString().slice(-4);
+      return substring1 + "..." + substring2;
     }
+  }
 
-    function chainChangedHandler() {
-        window.location.reload();
+  function createData(name, data) {
+    if (name === "Address") {
+      data = shortAddress(data);
     }
+    return { name, data };
+  }
 
-    function accountChangedHandler(accounts) {
-        setAccount(accounts[0]);
-        setBalance(null);
-        setDecimals(null);
-        setSymbol(null);
-        setTotalSupply(null);
-        setName(null);
-        getToken(accounts[0]);
-    }
+  const rows = [
+    createData("Address", account),
+    createData("Balance", balance + "  " + symbol),
+    createData("Name", name),
+    createData("Decimals", decimals),
+    createData("Total Supply", totalSupply),
+  ];
 
-    function shortAddress (account) {
-        if (account) {
-            let substring1 = account.toString().slice(0, 5);
-            let substring2 = account.toString().slice(-4);
-            return substring1 + "..." + substring2;
-        }
-    }
-
-    function createData(name, data) {
-        if (name === "Address") {
-            data = shortAddress(data);
-        }
-        return {name, data};
-    }
-
-    const rows = [
-        createData("Address", account),
-        createData("Balance", balance + "  " + symbol),
-        createData("Name", name),
-        createData("Decimals", decimals),
-        createData("Total Supply", totalSupply),
-    ]
-    
-    const connectForm = ( <Box sx={{
+  const connectForm = (
+    <Box
+      sx={{
         display: "flex",
         alignItems: "center",
-        backgroundColor: "#d7c1e8", 
+        backgroundColor: "#d7c1e8",
         borderRadius: "30px",
         minHeight: "200px",
         minWidth: "300px",
-        padding: "50px"
-    }}>
-        <Box sx={{textAlign: "center"}}>
+        padding: "50px",
+      }}
+    >
+      <Box sx={{ textAlign: "center" }}>
         <Typography variant="h5">Connect to Metamask Wallet</Typography>
-        <Button variant="contained" sx={{marginTop: "30px", backgroundColor: "#9979C1" }} onClick={connectHandler}>Connect</Button>
-        </Box>
-        <Typography variant="subtitle2" color="red">{errorMessage}</Typography>
-    </Box>);
+        <Button
+          variant="contained"
+          sx={{ marginTop: "30px", backgroundColor: "#9979C1" }}
+          onClick={connectHandler}
+        >
+          Connect
+        </Button>
+      </Box>
+      <Typography variant="subtitle2" color="red">
+        {errorMessage}
+      </Typography>
+    </Box>
+  );
 
-    const accountCard = (<Box sx={{
+  const accountCard = (
+    <Box
+      sx={{
         display: "flex",
         justifyContent: "space-around",
         alignItems: "center",
-        backgroundColor: "#d7c1e8", 
+        backgroundColor: "#d7c1e8",
         borderRadius: "30px",
-        padding: "30px"  
-    }}>
-        <Box>
-        <Card sx={{backgroundColor: "#d7c1e8", height: "350px", minWidth: "350px"}}>
-            <CardContent>
-            <Typography sx={{fontSize: "18px", fontWeight: "bold", textAlign: "center"}}>Account details:</Typography>
-            <Box sx={{marginTop: "20px"}}>
-            <Table>
-            <TableBody>
-                {rows.map((row) => {
-                    return <TableRow key={row.name}>
-                        <TableCell>{row.name}</TableCell>
-                        <TableCell>{row.data}</TableCell>
-                    </TableRow>
-                })}
-            </TableBody>    
-            </Table>
+        padding: "30px",
+      }}
+    >
+      <Box>
+        <Card
+          sx={{
+            backgroundColor: "#d7c1e8",
+            height: "300px",
+            minWidth: "350px",
+          }}
+        >
+          <CardContent>
+            <Typography
+              sx={{ fontSize: "18px", fontWeight: "bold", textAlign: "center" }}
+            >
+              Account details:
+            </Typography>
+            <Box sx={{ marginTop: "20px" }}>
+              <Table sx={{}}>
+                <TableBody >
+                  {rows.map((row) => {
+                    return (
+                      <TableRow key={row.name} hover={true} sx={{backgroundColor: "#e0d1ed", height: "10px"}}>
+                        <TableCell size="small" sx={{margin: "5px"}}>{row.name}</TableCell>
+                        <TableCell size="small" sx={{margin: "5px"}}>{row.data}</TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
             </Box>
-            </CardContent>
+          </CardContent>
         </Card>
-        </Box>
-        <Box sx={{paddingLeft: "20px"}}>
-        <Card sx={{backgroundColor: "#d7c1e8", height: "350px", minWidth: "250px"}}>
-        <CardContent> 
-        <Typography sx={{fontSize: "18px", fontWeight: "bold", textAlign: "center"}}>Transactions:</Typography>
-        </CardContent>
-        <Box sx={{display: "flex", flexDirection: "column", marginTop: "20px"}}>
-        <Button variant="contained" sx={{backgroundColor: "#9979C1" }} 
-        onClick={() => setIsActive(true)}
-        >Transfer</Button>
-        <Button variant="contained" sx={{backgroundColor: "#9979C1", marginTop: "15px" }}>Mint</Button>
-        </Box>       
+      </Box>
+      <Box sx={{ paddingLeft: "20px" }}>
+        <Card
+          sx={{
+            backgroundColor: "#d7c1e8",
+            height: "300px",
+            minWidth: "250px",
+          }}
+        >
+          <CardContent>
+            <Typography
+              sx={{ fontSize: "18px", fontWeight: "bold", textAlign: "center" }}
+            >
+              Transactions:
+            </Typography>
+          </CardContent>
+          <Box
+            sx={{ display: "flex", flexDirection: "column", marginTop: "10px" }}
+          >
+            <Button
+              variant="contained"
+              sx={{ backgroundColor: "#9979C1" }}
+              onClick={() => setIsActive(true)}
+            >
+              Transfer
+            </Button>
+            {/* <Button
+              variant="contained"
+              sx={{ backgroundColor: "#9979C1", marginTop: "15px" }}
+            >
+              Mint
+            </Button> */}
+          </Box>
         </Card>
-        </Box>
-    </Box>);
-
-    return  <div style={{backgroundColor: "#9979C1", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center"}}>
-        {account?  accountCard : connectForm}
-        <Backdrop open={isLoading}>
-        <CircularProgress color="inherit" />
-        </Backdrop>
-    <Box >
-        <Modal open={isActive} onClose={() => setIsActive(false)}sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-        <TransferForm />
-        </Modal>
+      </Box>
     </Box>
+  );
+
+  return (
+    <div
+      style={{
+        backgroundColor: "#9979C1",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      {account ? accountCard : connectForm}
+      <Backdrop open={isLoading}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <Box>
+        <Modal
+          open={isActive}
+          onClose={() => setIsActive(false)}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <TransferForm />
+        </Modal>
+      </Box>
     </div>
+  );
 }
 
 export default MetamaskConnect;
